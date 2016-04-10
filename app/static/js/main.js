a6fdd1bd94cba7ecb9eae1a91a0f99aca2312a92
Varun Munjeti <vrunjeti@gmail.com>
2016-04-09 23:02:03 -0500

    optimized for when no keywords are found, centered result image and text

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,11 +1,12 @@
 var imageSearchBaseURL = 'https://www.googleapis.com/customsearch/v1'
-var apiKey = 'AIzaSyAe2MVdSPyvFehxCdGvrrp98VIL5KeMC_A'
 var cx = '009967352923164700190:bh01lm5iclm'
+var apiKey = 'AIzaSyAe2MVdSPyvFehxCdGvrrp98VIL5KeMC_A'
 
 var recognition = new webkitSpeechRecognition()
 recognition.continuous = true
 
 recognition.onresult = function(event) {
+
   // continuous stream appends to the results array,
   // so the last element is the most recent
   var len = event.results.length
@@ -15,27 +16,35 @@ recognition.onresult = function(event) {
   $.ajax({
     url: '/api/getnoun/' + speechText,
     success: function(keyword){
-      // make a request to get the first google image result with the given keyword
-      $.ajax({
-        url: imageSearchBaseURL,
-        type: 'GET',
-        data: {
-          q: keyword,
-          num: 1,
-          imgSize: 'medium',
-          searchType: 'image',
-          key: apiKey,
-          cx: cx
-        }
-      })
-      .done(function(data) {
-        var imageLink = data.items[0].link
-        $('#img').attr('src', imageLink)
+
+      // if no keyword was found, set the picture to black
+      // this saves a request to the google image search api
+      if (keyword === 'black') {
+        $('#img').attr('src', 'http://a.tile.stamen.com/toner/12/653/1583.png')
         $('#text').text(speechText)
-      })
-      .fail(function(error) {
-        console.log('error', error)
-      })
+      } else {
+        // make a request to get the first google image result with the given keyword
+        $.ajax({
+          url: imageSearchBaseURL,
+          type: 'GET',
+          data: {
+            q: keyword,
+            num: 1,
+            imgSize: 'large',
+            searchType: 'image',
+            key: apiKey,
+            cx: cx
+          }
+        })
+        .done(function(data) {
+          var imageLink = data.items[0].link
+          $('#img').attr('src', imageLink)
+          $('#text').text(speechText)
+        })
+        .fail(function(error) {
+          console.log('error', error)
+        })
+      }
   }})
 }
 
